766358cbda38f38636118e6596b98fd2dcb52627
K Ceron <karemiceron@gmail.com>
2020-03-16 13:26:15 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,8 +9,8 @@ end
 # code your input_to_index and move method here!
 
 puts "Welcome to Tic Tac Toe! Please select a square by entering 1-9, 1 for the top left and 9 for the bottom right:"
-user_input = gets.to_i
 def input_to_index(user_input)
+  user_input = gets.to_i
   
 end
 
